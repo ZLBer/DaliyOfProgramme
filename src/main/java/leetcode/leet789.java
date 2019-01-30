3f42ed475d034be9b1860bd5aeb9946d01fde521
e <1098294815@qq.com>
2019-01-30 21:35:37 +0800

    leetcod刷题 1/30

---
+++ src/main/java/leetcode/leet789.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * Created by libin on 2019/1/30.
+ */
+public class leet789 {
+    public boolean escapeGhosts(int[][] ghosts, int[] t) {
+        int d = Math.abs(t[0]) + Math.abs(t[1]);
+        for (int[] g: ghosts)
+            if (d >= Math.abs(t[0] - g[0]) + Math.abs(t[1] - g[1]))
+                return false;
+        return true;
+    }
+}
---
+++ src/main/java/leetcode/leet94.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by libin on 2019/1/30.
+ */
+public class leet94 {
+
+    //mine 递归
+   /* List<Integer> result=new LinkedList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+   helper(root);
+   return result;
+    }
+    void  helper(TreeNode node){
+        if(node==null) return;
+        helper(node.left);
+       result.add(node.val);
+        helper(node.right);
+    }*/
+
+  //非递归
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode p = root;
+        while (!stack.isEmpty()||p!=null) {
+            if (p != null) {
+                stack.add(p);
+                p = p.left;
+            } else {
+                p = stack.pop();
+
+                result.add(p.val);
+                p = p.right;
+            }
+        }
+
+        return result;
+
+
+    }
+
+}
