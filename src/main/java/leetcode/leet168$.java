fe8b0fd793f6e530f3dc947d6d14badf803ce6c8
e <1098294815@qq.com>
2019-02-23 21:53:40 +0800

    leetcod刷题 2/23 晚

---
+++ src/main/java/leetcode/leet168$.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+import leetcode.LQTest.A;
+
+/**
+ * Created by libin on 2019/2/23.
+ */
+public class leet168$ {
+   //将数字转化成26进制
+//    public String convertToTitle(int n) {
+//       StringBuilder sb=new StringBuilder();
+//
+//
+//       if(n%26==0) {
+//           while (n > 26) {
+//
+//               sb.append((char) (n / 27 - 1 + 'A'));
+//               n /= 27;
+//           }
+//           return sb.reverse().toString();
+//       }
+//
+//
+//           while (n>26){
+//
+//           sb.append((char)(n%26-1+'A'));
+//           n/=26;
+//        }
+//        sb.append((char) (n-1+'A'));
+//
+//        return sb.reverse().toString();
+//    }
+
+ //   Instead of 1 -> A, 26 -> Z, we can assume that 0 -> A, 25 -> Z, and then here comes the base 26 representation, it's similar when you convert a number from base 10 to base 2
+ public String convertToTitle(int n) {
+     String res = "";
+     while(n != 0) {
+         char ch = (char)((n - 1) % 26 + 65);
+         n = (n - 1) / 26;
+         res = ch + res;
+     }
+     return res;
+ }
+
+   /* public String convertToTitle(int n) {
+        StringBuilder result = new StringBuilder();
+
+        while(n>0){
+            n--;
+            result.insert(0, (char)('A' + n % 26));
+            n /= 26;
+        }
+
+        return result.toString();
+    }*/
+}
---
+++ src/main/java/leetcode/leet242.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+import java源码学习.util.Map;
+
+import java.util.HashMap;
+
+/**
+ * Created by libin on 2019/2/18.
+ */
+public class leet242 {
+    //第一版
+   /* public boolean isAnagram(String s, String t) {
+     if(s.length()!=t.length()) return false;
+     HashMap<Character,Integer> yx=new HashMap<>();
+        //获取映射
+     for(int i=0;i<s.length();i++){
+             yx.put(s.charAt(i),yx.getOrDefault(s.charAt(i),0)+1);
+     }
+        for(int i=0;i<t.length();i++) {
+          //  System.out.println(i);
+         //   System.out.println(yx.getOrDefault(t.charAt(i),0));
+      if(yx.getOrDefault(t.charAt(i),0)==0)  return false;
+      else yx.put(t.charAt(i),yx.get(t.charAt(i))-1);
+        }
+        return true;
+    }*/
+    //第二版
+/*    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        HashMap<Character,Integer> yx=new HashMap<>();
+        //获取映射
+        for(int i=0;i<s.length();i++){
+            char a=s.charAt(i);
+            yx.put(a,yx.getOrDefault(a,0)+1);
+        }
+        for(int i=0;i<t.length();i++) {
+            char a=t.charAt(i);
+            if(yx.getOrDefault(a,0)==0)  return false;
+            else yx.put(a,yx.get(a)-1);
+        }
+        return true;
+    }*/
+
+//用数组代替map ，肯定更快
+    public boolean isAnagram(String s, String t) {
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet) if (i != 0) return false;
+        return true;
+    }
+
+
+}
