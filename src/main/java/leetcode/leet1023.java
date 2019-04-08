41ad2c6a53b60a139f2f863521f858b02674195f
e <1098294815@qq.com>
2019-04-08 20:23:27 +0800

    leetcod刷题 4/8 晚

--- src/main/java/leetcode/leet1023.java
+++ src/main/java/leetcode/leet1023.java
@@ -1,24 +1,54 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by libin on 2019/3/28.
  */
 public class leet1023 {
-    //全部遍历
-/*    public boolean queryString(String S, int N){
-        for(int i=N;i>=1;i--){
-            if(!S.contains(Integer.toBinaryString(i)))
-                return false;
+    public List<Boolean> camelMatch(String[] queries, String pattern) {
+
+        List<Boolean> result= new LinkedList<>();
+        char []paterns= pattern.toCharArray();
+        for (int i = 0; i < queries.length; i++) {
+           result.add(isMatch(queries[i].toCharArray(),paterns));
         }
-        return true;
-    }*/
-     // 只遍历一半
-//     The solution can be improve a half by checking from N to N/2.
-//    The reason is simply for every i < N/2, the binary string of 2*i will contain binary string of i. Thus we don't need to check for i < N/2
-    public boolean queryString(String S, int N) {
-        for (int i = N; i > N / 2; --i)
-            if (!S.contains(Integer.toBinaryString(i)))
-                return false;
-        return true;
+
+        return result;
     }
+
+   boolean isMatch(char [] querys,char [] patterns){
+        int index =0 ;
+
+       for (int i = 0; i < querys.length; i++) {
+           if(index<patterns.length&&querys[i]==patterns[index]){
+               index++;
+           }else if (querys[i]>='a'&&querys[i]<='z'){
+               continue;
+           }
+           else return false;
+       }
+
+
+       if(index==patterns.length) return  true;
+       else return false;
+   }
+
+   //用正则表达式做
+   /* public List<Boolean> camelMatch(String[] queries, String pattern) {
+        String newPattern = "[a-z]*" + String.join("[a-z]*", pattern.split("")) + "[a-z]*";
+        List<Boolean> ans = new ArrayList<>();
+        for (String q : queries) { ans.add(q.matches(newPattern)); }
+        return ans;
+    }*/
+    //正则+流
+
+    /*public List<Boolean> camelMatch(String[] queries, String pattern) {
+        String newPattern = "[a-z]*" + String.join("[a-z]*", pattern.split("")) + "[a-z]*";
+        return Arrays.stream(queries).map(q -> q.matches(newPattern)).collect(Collectors.toList());
+    }*/
 }
