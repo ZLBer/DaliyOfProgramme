61225d9f86f2b1bbd79bd3c71339e8c3d9e03622
e <1098294815@qq.com>
2019-04-16 21:31:54 +0800

    leetcod刷题 4/16 晚

---
+++ src/main/java/leetcode/leet1027.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by libin on 2019/4/16.
+ */
+public class leet1027 {
+    public int longestArithSeqLength(int[] A) {
+        List<Map<Integer,Integer>> list=new LinkedList<>();
+        for (int i = 1; i < A.length; i++) {
+             for(int j=i-1;i>0;j--){
+                 Map<Integer,Integer> map=new HashMap<>();
+                  
+             }
+        }
+
+
+return 0;
+    }
+}
