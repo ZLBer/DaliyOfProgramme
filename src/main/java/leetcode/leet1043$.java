9fbb88134e6e8339e0349c8c53b24564a8b813ce
e <1098294815@qq.com>
2019-05-14 22:01:56 +0800

    leetcod刷题 5/14

---
+++ src/main/java/leetcode/leet1043$.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class leet1043$ {
+    public int maxSumAfterPartitioning(int[] A, int K) {
+    int []dp=new int[A.length];
+
+    for(int i=0;i<A.length;i++){
+        int curMax=0;
+        for(int k=1;k<=K&&i-k+1>=0;k++){
+            //查找此段最大的值
+            curMax=Math.max(curMax,A[i-k+1]);
+            dp[i]=Math.max(dp[i],(i>=k?dp[i-k]:0)+curMax*k);
+        }
+    }
+      return  dp[A.length-1];
+    }
+}
--- src/main/java/leetcode/leet1043.java
+++
@@ -1,8 +0,0 @@
-package leetcode;
-
-public class leet1043 {
-    public int maxSumAfterPartitioning(int[] A, int K) {
-
-      return  0;
-    }
-}
