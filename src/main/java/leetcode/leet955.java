cba1d81e5816be67317be94671a3cf9ff8479524
e <1098294815@qq.com>
2019-05-31 17:55:20 +0800

    leetcod刷题 5/31

---
+++ src/main/java/leetcode/leet955.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class leet955 {
+    //贪心法
+    static  public int minDeletionSize(String[] A) {
+        int res = 0, n = A.length, m = A[0].length(), i, j;
+        //sort表示之前是否是有序的
+        boolean[] sorted = new boolean[n - 1];
+        for (j = 0; j < m; ++j) {
+            for (i = 0; i < n - 1; ++i) {
+                //若之前有序，则直接跳过
+                //若之前无序，则比较该处与之后 ：若之后大于之前，则需要删除这一列
+                if (!sorted[i] && A[i].charAt(j) > A[i + 1].charAt(j)) {
+                    res++;
+                    break;
+                }
+            }
+            //表示这一列删除了
+            if (i < n - 1) continue;
+            //表示此列未删除，需要更新最新的排序状态
+            for (i = 0; i < n - 1; ++i)
+                if (A[i].charAt(j) < A[i + 1].charAt(j))
+                    sorted[i] = true;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        minDeletionSize(new String[]{"xgab","xfaa","yfaa"});
+    }
+}
---
+++ src/main/java/leetcode/leet960$.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class leet960$ {
+    public int minDeletionSize(String[] A) {
+        //dp表示最大的递增串
+    int []dp=new int[A[0].length()];
+    Arrays.fill(dp,1);
+    int res=Integer.MAX_VALUE;
+    for(int j=0;j<A[0].length();j++){
+        for(int i=0;i<j;i++){
+            int k=0;
+            for(;k<A.length;k++){
+                if(A[k].charAt(i)>A[k].charAt(j)){
+                    break;
+                }
+                if(k==A.length-1&&dp[j]<dp[i]+1){
+                    dp[j]=dp[i]+1;
+                }
+            }
+        }
+        //n-最大=最小
+        res=Math.min(res,A[0].length()-dp[j]);
+    }
+    return  res;
+    }
+}
